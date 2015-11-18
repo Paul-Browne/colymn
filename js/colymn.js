27d614f434e7ea91205f8adb24a9f28f02822bae
paulbrowne <Paulmatthewbrowne@gmail.com>
2015-11-18 02:48:05 +0200

    added fixed width columns

--- js/colymn.js
+++ js/colymn.js
@@ -13,7 +13,7 @@
             }
         }), b;
     }
-    
+
     colymn = function() {
         var a = window.innerWidth,
             b = window.innerHeight,
@@ -26,59 +26,73 @@
             tl = 640,
             dt = 1024,
             hd = 1440;
-            
+
         // remove all clearing divs
         for (var z = 0; z < d.length; z++) {
             d[z].parentNode.removeChild(d[z]);
         }
+
+        function rep(pr) {
+            return RegExp(pr + "-(col|center)-", "g");
+        }
+
+        function rez(p) {
+            return RegExp(p + "-center-\\d+(px|em)", "g");
+        }
+
         function reg(pre, mid) {
             return RegExp(pre + "-" + mid + "-\\d+-\\d+", "g");
         }
+
         function testy(alpha) {
             return alpha.test(q[g].className);
         }
-        function matchy(gamma) {
-            return q[g].className.match(gamma).toString().match(/\d+/g);
+
+        function matchy2(gamma) {
+            return q[g].className.match(gamma).toString().match(/(\d+|px|em)/g);
         }
+
         function lfr(delta) {
-            var j = matchy(delta);
+            var j = matchy2(delta);
             var bv = j[0] / j[1] * ((100 + margin) - margin * j[1]) + margin * j[0] - margin;
             var par = parseFloat(q[g].parentNode.style.width);
             var tet = 100 / par * -(bv + margin) + "%";
             return tet;
         }
+
         function wid(qas) {
-            var n = matchy(qas);
+            var n = matchy2(qas);
             var amm = (n[0] * 100) / (n[1]) - ((n[1] - n[0]) * margin / n[1]) + "%";
             return amm;
         }
-        function svgg(c){
-            while(c.nodeName!=="svg"&&(c=c.parentNode)&&c.nodeName!=="svg"); 
-            return !!c; 
+
+        function svgg(c) {
+            while (c.nodeName !== "svg" && (c = c.parentNode) && c.nodeName !== "svg");
+            return !!c;
         }
         var mmm = 0;
         for (var x = 0; x < c.length; x++) {
-            if( !svgg(c[x]) ){
+            if (!svgg(c[x])) {
                 var q = c[x].children;
                 for (var g = 0; g < q.length; ++g) {
                     var m = q[g].style;
                     var joe = "xx";
-                    if ((a > pp) && (b > a) && (testy(reg("pp", "c\\w+")))) {
+                    if ((a > pp) && (b > a) && (testy(rep("pp")))) {
                         joe = "pp";
                     }
-                    if ((a > pl) && (a > b) && (testy(reg("pl", "c\\w+")))) {
+                    if ((a > pl) && (a > b) && (testy(rep("pl")))) {
                         joe = "pl";
                     }
-                    if ((a > tp) && (b > a) && (testy(reg("tp", "c\\w+")))) {
+                    if ((a > tp) && (b > a) && (testy(rep("tp")))) {
                         joe = "tp";
                     }
-                    if ((a > tl) && (a > b) && (testy(reg("tl", "c\\w+")))) {
+                    if ((a > tl) && (a > b) && (testy(rep("tl")))) {
                         joe = "tl";
                     }
-                    if ((a > dt) && (testy(reg("dt", "c\\w+")))) {
+                    if ((a > dt) && (testy(rep("dt")))) {
                         joe = "dt";
                     }
-                    if ((a >= hd) && (testy(reg("hd", "c\\w+")))) {
+                    if ((a >= hd) && (testy(rep("hd")))) {
                         joe = "hd";
                     }
                     var ss = reg(joe, "left"),
@@ -87,8 +101,9 @@
                         uu = reg(joe, "(left|right)"),
                         yy = reg(joe, "center"),
                         zz = reg(joe, "col"),
-                        ll = reg("\\w+", "(col|center)");
-                    
+                        ll = rep("\\w+"),
+                        vv = rez(joe);
+
                     // resets
                     if (testy(ll)) {
                         m.position = "relative";
@@ -98,7 +113,17 @@
                         m.float = "none";
                         m.width = 100 + "%";
                     }
-                    
+
+                    // if fixed width
+                    if (testy(vv)) {
+                        var we = matchy2(vv);
+                        m.display = "block";
+                        m.marginRight = "auto";
+                        m.marginLeft = "auto";
+                        m.float = "none";
+                        m.width = we[0] + we[1];
+                    }
+
                     // if center type
                     if (testy(yy)) {
                         m.width = wid(yy);
@@ -106,36 +131,36 @@
                         m.marginRight = "auto";
                         m.marginLeft = "auto";
                     }
-                    
+
                     // if column type
                     if (testy(zz)) {
                         m.width = wid(zz);
                         m.float = "left";
                         m.marginLeft = margin + "%";
-                        if (matchy(zz)[0] === "0") {
+                        if (matchy2(zz)[0] === "0") {
                             m.display = "none";
                         }
-                        
+
                         // if not left or right
                         if (!testy(uu)) {
-                        
+
                             // if not offset
-                            var yu = matchy(tt);
+                            var yu = matchy2(tt);
                             if (!yu[2]) {
                                 yu[2] = 0;
                                 yu[3] = 1;
                             }
-                            
+
                             // if first
                             if (mmm <= 0.01) {
                                 m.marginLeft = 0;
                             }
-                            
+
                             // if type offset
                             if (yu[2]) {
                                 var vat = yu[2] / yu[3] * ((100 + margin) - margin * yu[3]) + margin * yu[2];
                                 m.marginLeft = vat + margin + "%";
-                        
+
                                 // if first and offset
                                 if (mmm <= 0.01) {
                                     m.marginLeft = vat + "%";
@@ -172,3 +197,5 @@
     });
 })();
 
+
+colymn();
